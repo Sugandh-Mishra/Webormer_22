9a995d8f8f91d4b5df7487477764ef73f5179749
unknown <Sugandh-Mishra>
2023-11-14 18:33:10 +0530

    updating payment method

---
+++ frontend/src/components/Slider/sliderData.js
@@ -0,0 +1,22 @@
+const slider = [
+    {
+        title: "A platform for farmers to enhance the Agricultural system!",
+        description: "'' A revolututionary way to enhance farmer life ''",
+        tag: "",
+        image: "https://images.unsplash.com/photo-1523348837708-15d4a09cfac2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    },
+    {
+        title: "Solutions for a healthier And sustainable future",
+        description: "'' Agriculture is the fundamental of human life ''",
+        tag: "",
+        image: "https://images.unsplash.com/photo-1592982537447-7440770cbfc9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1499&q=80",
+    },
+    {
+        title: "We pray God Because they gave us a life And A farmer Give us food. Think........",
+        description: "''  ''",
+        tag: "",
+        image: "https://images.unsplash.com/photo-1574943320219-553eb213f72d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=682&q=80",
+    },
+];
+
+export default slider;
